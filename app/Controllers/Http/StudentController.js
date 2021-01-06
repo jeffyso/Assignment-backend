223b176534ee94e5ca2850e2554fd4b0d0f91256
jeffyso <jeffyzaja@gmail.com>
2021-01-06 11:57:02 +0700

    add-StudentController

--- app/Controllers/Http/StudentController.js
+++ app/Controllers/Http/StudentController.js
@@ -1,6 +1,17 @@
 'use strict'
 
+const Student = use('App/Models/Student')
+
 class StudentController {
+    async index ({ request }) {
+        let student = await Student.query().fetch()
+        return {status:200 , error:undefined ,data:student}
+    }
+    async show ({ request }) {
+        const {id} = request.params
+        const student = await Student.query().where({id:id}).fetch()
+        return {status:200 , error:undefined ,data:student}
+    }
 }
 
 module.exports = StudentController
