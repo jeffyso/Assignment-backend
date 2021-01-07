b37d92fcc30cb03078f7d667abfac05b95057afc
jeffyso <jeffyzaja@gmail.com>
2021-01-07 16:46:26 +0700

    edit-migration-university

--- database/migrations/1509835105778_university_schema.js
+++ database/migrations/1509835105778_university_schema.js
@@ -6,8 +6,8 @@ const Schema = use('Schema')
 class UniversitySchema extends Schema {
   up () {
     this.create('universities', (table) => {
-      table.increments('id')
-      table.string('name',255).notNullable()
+      table.increments('universities_id')
+      table.string('universities_name',255).notNullable()
       table.timestamps()
     })
   }
