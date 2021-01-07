02877e7036325edf82357517f63511896c653f5f
jeffyso <jeffyzaja@gmail.com>
2021-01-07 22:17:00 +0700

    edit-University-Model

--- app/Models/University.js
+++ app/Models/University.js
@@ -4,7 +4,10 @@
 const Model = use('Model')
 
 class University extends Model {
-      student(){
+    static get primaryKey() {
+        return 'universities_id'
+    }
+      students(){
           return this.belongsToMany('App/Models/Student').pivotTable('connects')
       }
 }
