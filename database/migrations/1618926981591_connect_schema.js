271e64617d7b61dec0dba19b0c50ec606b6bc09b
jeffyso <jeffyzaja@gmail.com>
2021-01-07 22:17:16 +0700

    edit-connect-migration

--- database/migrations/1618926981591_connect_schema.js
+++ database/migrations/1618926981591_connect_schema.js
@@ -8,14 +8,14 @@ class ConnectSchema extends Schema {
     this.create('connects', (table) => {
       table.increments()
       table.integer('student_id').unsigned()
-      table.integer('universities_id').unsigned()
+      table.integer('university_id').unsigned()
       
       table.foreign('student_id')
       .references('students.student_id')
       .onDelete('CASCADE') 
       .onUpdate('CASCADE') 
       
-      table.foreign('universities_id')
+      table.foreign('university_id')
       .references('universities.universities_id')
       .onDelete('CASCADE') 
       .onUpdate('CASCADE') 
