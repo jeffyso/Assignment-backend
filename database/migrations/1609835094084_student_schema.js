db576ddb176b974157e35a4e075d2773193710f0
jeffyso <jeffyzaja@gmail.com>
2021-01-07 16:46:44 +0700

    edit-migration-student

--- database/migrations/1609835094084_student_schema.js
+++ database/migrations/1609835094084_student_schema.js
@@ -6,16 +6,9 @@ const Schema = use('Schema')
 class StudentSchema extends Schema {
   up () {
     this.create('students', (table) => {
-      table.increments('id')
+      table.increments('student_id')
       table.string('first_name',255).notNullable()
       table.string('last_name',255).notNullable()
-      table.integer('universities_id').unsigned()
-
-      table.foreign('universities_id')
-      .references('universities.id')
-      .onDelete('CASCADE') 
-      .onUpdate('CASCADE') 
-
       table.timestamps()
     })
   }
